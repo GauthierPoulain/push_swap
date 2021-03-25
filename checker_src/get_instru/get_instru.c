7159756904d6bb3cb7e8c14cb914eb07d16d66ea
gauthier poulain <gapoulai@student.42lyon.fr>
2021-03-25 12:57:18 +0100

    fix des trucs

--- checker_src/get_instru/get_instru.c
+++ checker_src/get_instru/get_instru.c
@@ -6,7 +6,7 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 10:44:23 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/03/25 12:50:46 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/03/25 12:56:28 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	get_instru(t_checker *checker)
 			close_checker(CLOSE_ERROR);
 		else if (ret == 1)
 		{
-		run_instru(checker, *line);
-		if (PRINT_STACKS)
-			print_stacks(checker);
+			run_instru(checker, *line);
+			if (PRINT_STACKS)
+				print_stacks(checker);
 		}
 		free(*line);
 	}
