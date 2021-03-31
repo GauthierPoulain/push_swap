080cc61cb8185e48ee719be0f63731ba13966781
gauthier poulain <gapoulai@student.42lyon.fr>
2021-03-31 16:33:04 +0200

    salut

--- push_swap_src/sorting/six-hundred.c
+++ push_swap_src/sorting/six-hundred.c
@@ -6,7 +6,7 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 10:31:49 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/03/31 16:08:26 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/03/31 16:31:45 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,26 +34,24 @@ void		sort(t_checker *checker)
 {
 	int		min;
 	int		max;
-	int		closer;
 	
 	while (checker->stack_b.size > 0)
 	{
 		min = get_min(checker->stack_b);
 		max = get_max(checker->stack_b);
-		closer = get_closer(checker->stack_b, checker->stack_a.stack[0]);
 		if (checker->stack_b.stack[0] == min)
-			do_move(checker, "pa");
-		else if (checker->stack_b.stack[0] == max)
 		{
 			do_move(checker, "pa");
 			do_move(checker, "ra");
 		}
+		else if (checker->stack_b.stack[0] == max)
+			do_move(checker, "pa");
 		else
 			do_move(checker, "rb");
 	}
 }
 
-static bool	have_higher(t_stack stack, int min)
+bool	have_higher(t_stack stack, int min)
 {
 	int		i;
 
@@ -64,28 +62,40 @@ static bool	have_higher(t_stack stack, int min)
 	return (false);
 }
 
+bool	have_lower(t_stack stack, int max)
+{
+	int		i;
+
+	i = -1;
+	while (++i < stack.size)
+		if (stack.stack[i] > max)
+			return (true);	
+	return (false);
+}
+
 void	sort_six_to_hundred(t_checker *checker)
 {
 	int		med;
-	int		i;
 	int		size;
 
-
-	// print_stacks(checker);
 	size = checker->stack_a.size;
 	med = get_median(checker->stack_a);
-	i = 0;
 	while (have_higher(checker->stack_a, med))
 	{
 		if (checker->stack_a.stack[0] < med)
 			do_move(checker, "pb");
 		else
-		{
 			do_move(checker, "ra");
-			i++;
-		}
 	}
-	// print_stacks(checker);
 	sort(checker);
-	// print_stacks(checker);
+	while (have_lower(checker->stack_a, med))
+	{
+		if (checker->stack_a.stack[0] > med)
+			do_move(checker, "pb");
+		else
+			do_move(checker, "ra");
+	}
+	sort(checker);
+	while (!is_sorted(checker->stack_a))
+		do_move(checker, "ra");
 }
