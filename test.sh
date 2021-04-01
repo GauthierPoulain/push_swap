760f47dfec7a63d0f158c308d7907c27b0dbc66e
gauthier poulain <gapoulai@student.42lyon.fr>
2021-04-01 14:07:51 +0200

    update test

--- test.sh
+++ test.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 clear
 make
+ITER=$2
 MAX=0
-MIN=0
+MIN=-1
 COUNT=0
-MOY=0
+AVG=0
 TOTAL=0
-while true;
-do
-ARG=`ruby -e "puts (1..$1).to_a.shuffle.join(' ')"`
-# echo $ARG
-RET=$(./push_swap $ARG | ./checker $ARG)
-NB=$(./push_swap $ARG | wc -l)
-TOTAL=$(expr $TOTAL + $NB)
-COUNT=$(expr $COUNT + 1)
-MOY=$(expr $TOTAL / $COUNT)
-clear
-if [ $NB -gt $MAX ]
-then
-	MAX=$NB
-fi
-if [ $MIN == 0 ]
-then
-	MIN=$NB
-fi
-if [ $NB -lt $MIN ]
-then
-	MIN=$NB
-fi
-echo $RET
-echo $NB
-echo "min = $MIN"
-echo "max = $MAX"
-echo "avg = $MOY"
-if [ "$RET" != "OK" ]
-then
-	echo $ARG
-	read -n 1 k <&1
-fi
-done;
+while [ $COUNT -lt $ITER ]; do
+	ARG=$(ruby -e "puts (1..$1).to_a.shuffle.join(' ')")
+	RET=$(./push_swap $ARG | ./checker $ARG)
+	NB=$(./push_swap $ARG | wc -l)
+	TOTAL=$(expr $TOTAL + $NB)
+	COUNT=$(expr $COUNT + 1)
+	AVG=$(expr $TOTAL / $COUNT)
+	if [ $NB -gt $MAX ]; then
+		MAX=$(expr $NB)
+	fi
+	if [ $MIN == -1 ]; then
+		MIN=$(expr $NB)
+	fi
+	if [ $NB -lt $MIN ]; then
+		MIN=$(expr $NB)
+	fi
+	clear
+	echo "$COUNT / $ITER"
+	echo $RET
+	echo $NB
+	echo "min = $MIN"
+	echo "max = $MAX"
+	echo "avg = $AVG"
+	if [ "$RET" != "OK" ]; then
+		echo $ARG
+		read -n 1 k <&1
+	fi
+done
